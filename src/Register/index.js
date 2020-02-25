65507267af0c8a49d79dbd7149737b190f585019
Walter Sylvester <walter_sylvester@msn.com>
2020-02-25 13:04:07 -0700

    set up select department from a dropdown list form on registration

--- src/Register/index.js
+++ src/Register/index.js
@@ -3,24 +3,13 @@ import { Form, Button, Message, Grid, Header, Icon, Segment, Dropdown, Select } 
 import { Link } from 'react-router-dom';
 
 
-const departmentOptions = [
-  {
-    key: 'IT',
-    text: 'IT',
-    value: 'IT'
-  },
-
-  {
-    key: 'DATA',
-    text: 'DATA',
-    value: 'DATA'
-  },
-  
-  {
-    key: 'HR',
-    text: 'HR',
-    value: 'HR'
-  }  
+const options = [
+  { key: 1, text: 'Information Technology', value: 'IT' },
+  { key: 2, text: 'Human Resources', value: 'HR' },
+  { key: 3, text: 'Finance', value: 'Fin' },
+  { key: 4, text: 'Sales', value: 'Sales' },
+  { key: 5, text: 'Marketing', value: 'MKT'},
+  { key: 6, text: 'Engineering', value: 'ENG'},
 ]
   
 
@@ -40,16 +29,22 @@ class Register extends Component {
 	
 	// Handling of form value change
 	handleChange = (e) => {
-		this.setState({
+		e.preventDefault();
+    this.setState({
 			[e.currentTarget.name]: e.currentTarget.value
-
 		})
 	}
+
+  handleSelectDeptChange = (e, { value }) => {
+    this.setState({ 
+      department: value
+    })
+  }
 
 	// Submission of register in form
 	handleSubmit = async (e) => {
 		e.preventDefault();
-		console.log('hello', this.state);
+		console.log('hello', this.state);// ***THIS NEEDS TO COMMENTED OUT IN PRODUCTION***
 		const registrationUrl = `${process.env.REACT_APP_API_URL}/api/v1/users/register`; // localhost:8000/api/v1/users/register
     	// this is users.  this matches flask app.py: app.register_blueprint(user, url_prefix='/api/v1/users')
     	const registerResponse = await fetch(registrationUrl, {
@@ -76,6 +71,7 @@ class Register extends Component {
 	}
 
 	render() {
+    // const { value } = this.state
 		return (
 			<Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
     <Grid.Column style={{ maxWidth: 450 }}>
@@ -92,17 +88,13 @@ class Register extends Component {
           	placeholder='Name' 
           	onChange={this.handleChange} 
           	required/>
-          <Form.Input
-          	name = 'department'
-          	type='text'
-            // iconPosition='left'
-            // icon='users'
-            placeholder='Department'
+          <Form.Select
             fluid
-            selection
-            // options={departmentOptions}
-            onChange={this.handleChange} 
-            required />
+            options={options}
+            placeholder='Choose a Department'
+            onChange={this.handleSelectDeptChange}
+            // value={value}
+            required/>
           <Form.Input 
           	name = 'email'
           	type='email'
